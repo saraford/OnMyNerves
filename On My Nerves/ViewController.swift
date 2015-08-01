caa45dc993b8d5023e7fcbfe56d825130b79acc1
Sara Ford <saraford@users.noreply.github.com>
2015-07-31 20:07:32 -0700

    Initial Commit

---
+++ On My Nerves/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  On My Nerves
+//
+//  Created by Sara Ford on 7/31/15.
+//  Copyright (c) 2015 Sara Ford. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
